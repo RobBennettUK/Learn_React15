63f03c9bcc51cd4e9360ce6cdf71e09b177485da
RobBennettUK <38293815+RobBennettUK@users.noreply.github.com>
2020-11-13 12:46:41 +0000

    Lesson Completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,30 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
+// Each child in a list should have a unique "key" prop.
+// We cannot access this key. It is not a prop!
+// If we want to access it, we should create another prop
+
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      id={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      {contacts.map(createCard)}
 
-      <Card
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -24,9 +42,12 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
 
 export default App;
+
+// Mapping is used to map a javascript object. It will go through each element and pass it to the function that is using it
+// This is why we create a new function to place within map()
